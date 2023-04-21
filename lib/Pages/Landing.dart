beda8d322e0f42611a149bbf061bb2a9a2b0d210
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-04-21 18:02:17 +0530

    initial commit

---
+++ lib/Pages/Landing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ,
+    );
+  }
+}
