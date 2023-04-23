ec2be62048196691315c892ce8ff9735192caafb
satvshr <112589278+satvshr@users.noreply.github.com>
2023-04-23 19:14:08 +0530

    almost done with main menu card

--- lib/Widgets/RestaurantCard.dart
+++ lib/Widgets/RestaurantCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RestaurantCard extends StatefulWidget {
   String entry;
@@ -20,33 +21,64 @@ class _RestaurantCardState extends State<RestaurantCard> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(13.0),
       child: Card(
+        color: Color(0xffE01313),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(40.0),
         ),
         elevation: 16.0,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                height: 150,
-                width: 400,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: urlImages.length.isFinite && urlImages.length > 0
-                      ? urlImages.length.toInt()
-                      : 0,
-                  itemBuilder: (context, index) {
-                    final urlImage = urlImages[index];
-                    return buildImage(urlImage, index);
-                  },
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 3),
+                child: SizedBox(
+                  height: 150,
+                  width: 400,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: urlImages.length.isFinite && urlImages.length > 0
+                        ? urlImages.length.toInt()
+                        : 0,
+                    itemBuilder: (context, index) {
+                      final urlImage = urlImages[index];
+                      return buildImage(urlImage, index);
+                    },
+                  ),
                 ),
               ),
-            ),
-            Text('Casablanca'),
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(22, 0, 0, 0),
+                child: Text(
+                  'Casablanca',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(17, 0, 0, 8),
+                child: RatingBar.builder(
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  itemCount: 5,
+                  itemSize: 22.5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
