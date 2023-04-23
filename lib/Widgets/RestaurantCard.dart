52e2879856c868f5d827f9678c1e6c66f1898220
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-04-23 21:11:50 +0530

    meh

--- lib/Widgets/RestaurantCard.dart
+++ lib/Widgets/RestaurantCard.dart
@@ -9,11 +9,18 @@ class RestaurantCard extends StatefulWidget {
 }
 
 class _RestaurantCardState extends State<RestaurantCard> {
+  final urlImages = [
+    'https://lh3.googleusercontent.com/p/AF1QipNHr_KPpZ0orf3tXeuOjtChdiFhlgF66u_nUKDD=s1360-w1360-h1020',
+    'https://img.restaurantguru.com/cfd6-Casablanca-Vallikavu-interior.jpg',
+    'https://10619-2.s.cdn12.com/rests/small/w285/102_505848815.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Padding(
+<<<<<<< HEAD
         padding: const EdgeInsets.all(10.0),
         child: Container(
           height: height * 0.22,
@@ -22,5 +29,49 @@ class _RestaurantCardState extends State<RestaurantCard> {
           decoration: BoxDecoration(
               color: Colors.white, borderRadius: BorderRadius.circular(20)),
         ));
+=======
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+        elevation: 16.0,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 150,
+                width: 400,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: urlImages.length.isFinite && urlImages.length > 0
+                      ? urlImages.length.toInt()
+                      : 0,
+                  itemBuilder: (context, index) {
+                    final urlImage = urlImages[index];
+                    return buildImage(urlImage, index);
+                  },
+                ),
+              ),
+            ),
+            Text('Casablanca'),
+          ],
+        ),
+      ),
+    );
+>>>>>>> b756859 (changes)
   }
+
+  Widget buildImage(String urlImage, int index) => Container(
+    margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Image.network(
+        urlImage,
+      ),
+    ),
+  );
+
+
 }
