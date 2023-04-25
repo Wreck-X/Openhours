efb719d57f3ca099dcb83f54df001878829c0d9a
satvshr <112589278+satvshr@users.noreply.github.com>
2023-04-25 14:00:16 +0530

    can now swipe through pages of images instead

--- lib/Widgets/RestaurantCard.dart
+++ lib/Widgets/RestaurantCard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:page_indicator/page_indicator.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class RestaurantCard extends StatefulWidget {
   String entry;
@@ -10,11 +12,9 @@ class RestaurantCard extends StatefulWidget {
 }
 
 class _RestaurantCardState extends State<RestaurantCard> {
-  final urlImages = [
-    'https://lh3.googleusercontent.com/p/AF1QipNHr_KPpZ0orf3tXeuOjtChdiFhlgF66u_nUKDD=s1360-w1360-h1020',
-    'https://img.restaurantguru.com/cfd6-Casablanca-Vallikavu-interior.jpg',
-    'https://10619-2.s.cdn12.com/rests/small/w285/102_505848815.jpg'
-  ];
+  final urlImages = [    'https://lh3.googleusercontent.com/p/AF1QipNHr_KPpZ0orf3tXeuOjtChdiFhlgF66u_nUKDD=s1360-w1360-h1020',    'https://img.restaurantguru.com/cfd6-Casablanca-Vallikavu-interior.jpg',    'https://10619-2.s.cdn12.com/rests/small/w285/102_505848815.jpg'  ];
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,19 +34,37 @@ class _RestaurantCardState extends State<RestaurantCard> {
             children: [
               Padding(
                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 3),
-                child: SizedBox(
-                  height: 150,
-                  width: 400,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: urlImages.length.isFinite && urlImages.length > 0
-                        ? urlImages.length.toInt()
-                        : 0,
-                    itemBuilder: (context, index) {
-                      final urlImage = urlImages[index];
-                      return buildImage(urlImage, index);
-                    },
-                  ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 150,
+                      width: 400,
+                      child: PageView.builder(
+                        onPageChanged: (index) {
+                          setState(() {
+                            _currentIndex = index;
+                          });
+                        },
+                        itemCount: urlImages.length,
+                        itemBuilder: (context, index) {
+                          final urlImage = urlImages[index];
+                          return buildImage(urlImage, index);
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    DotsIndicator(
+                      dotsCount: urlImages.length,
+                      position: _currentIndex.toDouble(),
+                      decorator: DotsDecorator(
+                        color: Colors.grey[300]!,
+                        activeColor: Colors.white,
+                        spacing: EdgeInsets.all(3),
+                        activeSize: Size(10, 10),
+                        size: Size(6, 6),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Padding(
@@ -85,7 +103,7 @@ class _RestaurantCardState extends State<RestaurantCard> {
   }
 
   Widget buildImage(String urlImage, int index) => Container(
-        margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+        margin: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20.0),
           child: Image.network(
