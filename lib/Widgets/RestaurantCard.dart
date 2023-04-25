ddc4449ce4c98c29e74dfab20a53dd9793924af0
satvshr <112589278+satvshr@users.noreply.github.com>
2023-04-25 14:04:49 +0530

    added an GestureDetector

--- lib/Widgets/RestaurantCard.dart
+++ lib/Widgets/RestaurantCard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:openhours/Pages/RestaurantDetails.dart';
 import 'package:page_indicator/page_indicator.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 
@@ -22,80 +23,88 @@ class _RestaurantCardState extends State<RestaurantCard> {
     double width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.all(13.0),
-      child: Card(
-        color: Color(0xffE01313),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40.0),
-        ),
-        elevation: 16.0,
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 3),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 150,
-                      width: 400,
-                      child: PageView.builder(
-                        onPageChanged: (index) {
-                          setState(() {
-                            _currentIndex = index;
-                          });
-                        },
-                        itemCount: urlImages.length,
-                        itemBuilder: (context, index) {
-                          final urlImage = urlImages[index];
-                          return buildImage(urlImage, index);
-                        },
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RestaurantDetails()));
+        },
+        child: Card(
+          color: Color(0xffE01313),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          elevation: 16.0,
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 3),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 150,
+                        width: 400,
+                        child: PageView.builder(
+                          onPageChanged: (index) {
+                            setState(() {
+                              _currentIndex = index;
+                            });
+                          },
+                          itemCount: urlImages.length,
+                          itemBuilder: (context, index) {
+                            final urlImage = urlImages[index];
+                            return buildImage(urlImage, index);
+                          },
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 8),
-                    DotsIndicator(
-                      dotsCount: urlImages.length,
-                      position: _currentIndex.toDouble(),
-                      decorator: DotsDecorator(
-                        color: Colors.grey[300]!,
-                        activeColor: Colors.white,
-                        spacing: EdgeInsets.all(3),
-                        activeSize: Size(10, 10),
-                        size: Size(6, 6),
+                      SizedBox(height: 8),
+                      DotsIndicator(
+                        dotsCount: urlImages.length,
+                        position: _currentIndex.toDouble(),
+                        decorator: DotsDecorator(
+                          color: Colors.grey[300]!,
+                          activeColor: Colors.white,
+                          spacing: EdgeInsets.all(3),
+                          activeSize: Size(10, 10),
+                          size: Size(6, 6),
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(22, 0, 0, 0),
+                  child: Text(
+                    'Casablanca',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(22, 0, 0, 0),
-                child: Text(
-                  'Casablanca',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+                    textAlign: TextAlign.left,
                   ),
-                  textAlign: TextAlign.left,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(17, 0, 0, 8),
-                child: RatingBar.builder(
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  itemCount: 5,
-                  itemSize: 22.5,
-                  itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(17, 0, 0, 8),
+                  child: RatingBar.builder(
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    itemCount: 5,
+                    itemSize: 22.5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
                   ),
-                  onRatingUpdate: (rating) {
-                    print(rating);
-                  },
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
